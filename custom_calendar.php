4f8bc3dd3e493a1575dbf4f762ea524d02c48f63
e-farkas <elizabeth.a.farkas@gmail.com>
2019-05-30 16:27:24 -0700

    fancy calendar stuff

---
+++ custom_calendar.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>tanbecks@gmail.com</title>
+<script nonce="ErAETCGgd4EJag15WLFHwQ">gcal$perf$serverTime=37;gcal$perf$headStartTime=new Date().getTime()</script>
+<style type="text/css" nonce="ErAETCGgd4EJag15WLFHwQ">body{margin:0;padding:0;overflow-y:hidden;}
+        html{overflow-y:hidden;}</style>
+
+
+
+<script type="text/javascript" nonce="ErAETCGgd4EJag15WLFHwQ">
+  function _DumpException(e) { throw e; }
+  </script>
+<script type="text/javascript" src="https://calendar.google.com/calendar/_/scs/calendar-static/_/js/k=calendar.embed.en.zGuNrZJHWR4.O/d=1/rs=ANwU0p4j3f67cMQhgef40SEpC4BhZSF0Ww/m=embed" nonce="ErAETCGgd4EJag15WLFHwQ"></script>
+<script nonce="ErAETCGgd4EJag15WLFHwQ">function _onload() {window._init({"features":[1,1,1,1,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,1,0,0,0,0,0,1,0,1,1,0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,1,1,0,1,0,0,0,1,1,0,1,1,1,0,0,0,1,1,1,0,0,1,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,0,0,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,0,0,0],"loggedin":false,"cids":{"tanbecks@gmail.com":{"color":"#711616","access":20,"title":"tanbecks@gmail.com"}},"view":"month","weekstart":0,"format24hour":false,"dateFieldOrder":0,"preloadStart":"20190427","preloadEnd":"20190607","container":"container","baseUrl":"/","imagePath":"//calendar.google.com/googlecalendar/images/","timezone":"Europe/Istanbul","timezoneLocalized":"Turkey Time","timezoneOffsetMs":10800000,"nowMs":"1558988123655","showNavigation":false,"showDateMarker":true,"showTabs":true,"showCalendarMenu":false,"showCurrentTime":true,"showTz":true,"showPrintButton":false,"showElementsLogo":false,"collapseAllday":false,"showSubscribeButton":true,"embedStyle":"WyJhdDplbWI6c3QiXQo\u003d","proxyUrl":"https://clients6.google.com","calendarApiVersion":"v3","developerKey":"AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"});}</script>
+<script type="text/javascript" nonce="ErAETCGgd4EJag15WLFHwQ">
+      var pageLoaded_ = false;
+      var clientLibraryLoaded_ = false;
+
+      function clientLibraryLoaded() {
+        clientLibraryLoaded_ = true;
+        if (pageLoaded_) _onload();
+      }
+
+      function pageLoaded() {
+        pageLoaded_ = true;
+        if (clientLibraryLoaded_) _onload();
+      }
+    </script>
+<script type="text/javascript" src="https://apis.google.com/js/client.js?onload=clientLibraryLoaded" nonce="ErAETCGgd4EJag15WLFHwQ"></script>
+<style type="text/css" nonce="ErAETCGgd4EJag15WLFHwQ">
+   
+  </style><link type="text/css" rel="stylesheet" href="schedule.min.css"></head>
+<body style="background-color:#fff"><script type="text/javascript" nonce="ErAETCGgd4EJag15WLFHwQ">
+    document.addEventListener('DOMContentLoaded', pageLoaded);
+  </script>
+
+
+
+<noscript><p></p>
+Your browser does not appear to support JavaScript, but this page needs to use JavaScript to display correctly.
+You can visit the HTML-only version of this page at:
+<a href="https://calendar.google.com/calendar/htmlembed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;hl=en&amp;bgcolor=%23FFFFFF&amp;src=tanbecks@gmail.com&amp;color=%23711616&amp;ctz=Europe/Istanbul">https://calendar.google.com/calendar/htmlembed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;hl=en&amp;bgcolor=%23FFFFFF&amp;src=tanbecks@gmail.com&amp;color=%23711616&amp;ctz=Europe/Istanbul</a></noscript>
+<div id="container" style="width:100%" class="locale-en "></div></body></html>
